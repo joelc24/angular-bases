298ad2be15e8da6fe55dc76cc388126708239618
Joel cues <libra-marriaga@hotmail.com>
2023-02-14 18:14:35 -0500

    heroe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './components/contador/contador.component';
+import { HeroeComponent } from './components/heroes/heroe/heroe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent,
   ],
   imports: [
     BrowserModule
